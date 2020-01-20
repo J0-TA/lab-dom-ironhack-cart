2db4035b215ee1d2969e2dc3295a8115588df001
Juan Carlos <jcarlosriego@gmail.com>
2020-01-20 21:58:14 +0100

    Done all the funcionalities, but plenty of bugs

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,48 +1,65 @@
 let $cart = document.querySelector('#cart tbody');
 let $calc = document.getElementById('calc');
 
+// Here I set an onchange function who calculates the subtotal when qty input field
+// changes and print it in the HTML
 
-//Here I make a Product class to construct all the new products.
+function setOnChanges () {
+  let price, rowSubtotal;
+  document.querySelectorAll(`.product`).forEach(product => { 
+    product.querySelector(`.qty label input`).onchange = function (e) {
+      price = +product.querySelector(`.pu span`).innerText;
+      rowSubtotal = +(e.target.value * price).toFixed(2);
+      product.querySelector(`.subtot span`).innerHTML = rowSubtotal;
+    };
+  });
+}
+setOnChanges ()
 
-class Product {
-  constructor (nameParam, priceParam, amountParam) {
-    this.name = nameParam;
-    this.price = parseFloat(priceParam).toFixed(2);
-    this.amount = parseFloat(amountParam);
-    this.subtotal = (this.price * this.amount).toFixed(2);
-  }
-};
+//Here I define a function who paint a new row from the DOM and give the properties of the other product's rows.
 
-//Here I define my initial array of products
-
-let cartProducts = [new Product (`IronBubble-head`,25,5)];
-
-// Here I define the function to paint the cart.
-
-function paintCart() {
-  $cart.innerHTML = "";
-  for (let i = 0; i < cartProducts.length; i++) {
+function setCreate () {
+  document.querySelector(`#create`).onclick = function () {
     let cartNewElement = document.createElement("tr")
     cartNewElement.className = "product";
-    cartNewElement.innerHTML = `<td class="name"><span>${cartProducts[i].name}</span></td>
-    <td class="pu"><span>$</span>${cartProducts[i].price}</span></td>
-    <td class="qty"><label><input type="number" value="${cartProducts[i].amount}" id="${i}" min="0"/></label></td>
-    <td class="subtot"><span>$</span><span class="subtotals">${cartProducts[i].subtotal}</span></td>
+    cartNewElement.innerHTML = `<td class="name"><span>${document.querySelector(`.text input`).value}</span></td>
+    <td class="pu"><span>$</span>${parseFloat(document.querySelector(`.number input`).value).toFixed(2)}</span></td>
+    <td class="qty"><label><input type="number" value="0" min="0"/></label></td>
+    <td class="subtot">$<span></span></td>
     <td class="rm"><button class="btn btn-delete">Delete</button></td>`
     $cart.appendChild(cartNewElement);
-  }
+    setOnChanges ()
+    setDelete ()
+  } 
+} 
+setCreate()
+
+// Here a I define a function to give de delete buttons the capacity to delete his own row.
+
+function setDelete () {
+  let rowToDelete
+  document.querySelectorAll(`pruduct`).forEach(product => {
+    product.querySelector(`btn btn-delete`).onclick = function (e) {
+    e.parentNode.parentNode.removeChild(e.parentNode);
+    }
+  })
 }
+setDelete ()
+
+// Here I calculate all the amounts together.
+
 function calcAll() {
-  paintCart();
-  var i = 1;
-  var sum = 0;
-  while (i < $cart.rows.length) {
-      sum += parseFloat(table.rows[i].cells[3].innerHTML)
-      i++;
-  }
-  document.querySelector('h2').innerHTML = `${sum}`;
+  let price = 0;
+  let productPrice, productQty, rowSubtotal;
+  
+  document.querySelectorAll(`.product`).forEach(product => {
+  productPrice = +product.querySelector(`.pu span`).innerText;
+  productQty = product.querySelector(`.qty label input`).value;
+  rowSubtotal = productPrice * productQty;
+  product.querySelector(`.subtotal`).innerHTML = `${rowSubtotal}$`;
+  price += rowSubtotal;
+  });
+  document.querySelector(`h2 span`).innerHTML = `${price}`;
 }
 
-$calc.onclick = calcAll;
-
-
+$calc.onclick = calcAll();
